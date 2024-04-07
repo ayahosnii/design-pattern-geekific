a05b6f13e44408f866db733abc4be7d941ee8feb
Aya Hosny <aya.hosni@id3m.com>
2024-04-07 09:05:27 +0200

    Main

---
+++ Structural/Bridge/Main.java
@@ -0,0 +1,31 @@
+package Bridge;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an American style pizza
+        Pizza americanPizza = new PepperoniPizza();
+        Restaurant americanRestaurant = new AmericanRestaurant(americanPizza);
+
+        // Prepare the American style pizza
+        americanRestaurant.addTopping();
+        americanRestaurant.addSauce();
+        americanRestaurant.makeCrust();
+
+        // Assemble and check the quality of the American style pizza
+        americanPizza.assemble();
+        americanPizza.qualityCheck();
+
+        // Create an Italian style pizza
+        Pizza italianPizza = new ItalianPizza();
+        Restaurant italianRestaurant = new ItalianRestaurant(italianPizza);
+
+        // Prepare the Italian style pizza
+        italianRestaurant.addTopping();
+        italianRestaurant.addSauce();
+        italianRestaurant.makeCrust();
+
+        // Assemble and check the quality of the Italian style pizza
+        italianPizza.assemble();
+        italianPizza.qualityCheck();
+    }
+}
